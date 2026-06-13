e946db5a9ac2a4d873d9b8ab5e2f198ae6d6f441
mrdtd2k6-dotcom <mr.dtd2k6@gmail.com>
2026-06-13 17:00:29 +0700

    Update baiV_4_3.m

--- baiV_4_3.m
+++ baiV_4_3.m
@@ -1,32 +1,27 @@
-a = 0.2;    
-b = 1.4;    
-H = 3;      
+a = 0.25;    
+b = 1.0;     
+H = 3;       
+[X, Y] = meshgrid(-6:0.01:6, -6:0.01:6);
 
-[X, Y] = meshgrid(-6:0.005:6, -6:0.005:6); 
-mask_cross = false(size(X));
 
-for m = -4:4
-    for n = -4:4
-        if m == 0 || n == 0
-            continue;
+mask = false(size(X));
+for m = -8:8
+    for n = -8:8
+        if m == 0 || (n == 0 && m <= 0)
+            continue
         end
-        
-        xc = n * b;
+        xc = n * b; 
         yc = m * b;
-        mask_cross((X - xc).^2 + (Y - yc).^2 <= a^2) = true;
+        mask((X - xc).^2 + (Y - yc).^2 <= a^2) = true;
     end
 end
-
-mask_dilated = conv2(double(mask_cross), ones(3), 'same') > 0;
-boundary = mask_dilated & ~mask_cross;
-
+mask_dilated = conv2(double(mask), ones(3), 'same') > 0;
+boundary = mask_dilated & ~mask;
 Z = zeros(size(X));
-Z(mask_cross) = H;         
-Z(boundary) = 0;     
+Z(mask) = H; 
+Z(boundary) = 0;
 
-figure('Color', 'w');
 surf(X, Y, Z, 'LineStyle', 'none');
-colormap([0.2 0.2 0.7; 1 1 0]);
-title('Waveguide chữ thập (Mạng vuông)');
-axis equal; grid on; view(3);
+colormap(gca, [0.2 0.2 0.7; 1 1 0]);
+axis equal; axis([-6 6 -6 6 0 H]); grid on; view(3);
 camlight('headlight'); lighting gouraud;
